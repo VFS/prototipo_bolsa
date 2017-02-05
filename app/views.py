01753e9629812e1f2bd3c0db68de977a4eee325e
Victor Fischer Scattone <v+git@vfs.io>
2017-02-05 11:14:36 -0200

    Remove debug comments

--- app/views.py
+++ app/views.py
@@ -12,18 +12,6 @@ s_h = dtex.get_historical('2016-11-11', '2017-01-20')
 # The list comes in decrescent order, so we need to reverse
 s_h.reverse()
 
-# date = ['2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05']
-# date.insert(0, 'Date')
-
-# stock = [30, 200, 100, 400, 150]
-# stock.insert(0, 'Stock')
-
-# cdi = [50, 400, 14, 40, 30]
-# cdi.insert(0, 'CDI')
-
-
-# response = [date, stock, cdi]
-
 
 @app.route('/')
 @app.route('/index')
