793ce033435d9ea8c8e715837589b177d2e43c14
Victor Fischer Scattone <v+git@vfs.io>
2017-02-05 12:11:07 -0200

    Simplify debug

--- app/get_cdi.py
+++ app/get_cdi.py
@@ -4,10 +4,13 @@ from datetime import datetime, timedelta
 
 base_url = 'ftp://ftp.cetip.com.br/IndiceDI/'
 
+# CDI data will live on a dict
+cdi_dict = dict()
+
 
 # Fetch a file from a specific day and return it's data as a dict
-def get_CDI_by_date(day):
-    day = day.strftime('%Y%m%d')
+def get_CDI_by_date(d):
+    day = d.strftime('%Y%m%d')
     url = base_url + day + '.txt'
 
     try:
@@ -17,14 +20,19 @@ def get_CDI_by_date(day):
         cdi = format(cdi, '.2f')
         return cdi
 
-    except urllib.error.URLError as e:
-        print(e.reason)
+    except:
+        print('Deu ruim')
 
 
 # iterates trough a date range, ignoring weekends
 def get_CDI_by_range(start, end):
     start = datetime.strptime(start, '%Y-%m-%d')
     end = datetime.strptime(end, '%Y-%m-%d')
+
+    if end > datetime.today():
+        end = datetime.today()
+        print("You can't fetch future information!")
+
     delta = timedelta(days=1)
     d = start
     diff = 0
@@ -37,4 +45,9 @@ def get_CDI_by_range(start, end):
         d += delta
 
 
-print(get_CDI_by_range('2016-02-10','2017-02-10'))
+print(get_CDI_by_range('2017-01-20', '2017-02-10'))
+
+print('\n\n====\n\n')
+
+
+print(cdi_dict)
