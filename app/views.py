9cf3b394d29eed7e1286d66008f83601d2187a0d
Victor Fischer Scattone <v+git@vfs.io>
2017-02-05 21:27:24 -0200

    Typo

--- app/views.py
+++ app/views.py
@@ -47,7 +47,7 @@ def create_stock_cdi_response(stock, start_date, end_date):
         el['Adj_Close_Percent'] = adj * adj_close_percent
         el_date = el['Date']
         el['CDI'] = r[el_date]
-        el['CDI_percent'] = float(r[el_date]) * cdi_percent
+        el['CDI_Percent'] = float(r[el_date]) * cdi_percent
     print(type(response))
     #print(response)
     #print(r.get())
